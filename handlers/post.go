15017935bdf50bcd068795b3a631f8846ac9cd38
Koushik Chakrabarty <koushich@in.ibm.com>
2021-06-29 19:20:18 +0530

    google logging both structured and standard

--- handlers/post.go
+++ handlers/post.go
@@ -4,6 +4,8 @@ import (
 	"employee-api/data"
 	"encoding/json"
 	"net/http"
+
+	"cloud.google.com/go/logging"
 )
 
 // swagger:route POST /emps employees createEmployee
@@ -17,9 +19,21 @@ func (e *Employees) CreateSingleEmp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var employee data.Employee
 	_ = json.NewDecoder(r.Body).Decode(&employee)
+	e.l.StandardLogger(logging.Debug).Println("Requst Payload to the service")
+	e.l.Log(logging.Entry{
+		// Log anything that can be marshaled to JSON.
+		Payload:  employee,
+		Severity: logging.Debug,
+	})
 	employee.ID = data.Count
 	data.Count++
 	data.Emps = append(data.Emps, employee)
 	json.NewEncoder(w).Encode(employee)
+	e.l.StandardLogger(logging.Debug).Println("Response Payload to the service")
+	e.l.Log(logging.Entry{
+		// Log anything that can be marshaled to JSON.
+		Payload:  employee,
+		Severity: logging.Debug,
+	})
 
 }
